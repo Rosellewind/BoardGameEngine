b0b1ddbc1b45155700ce31bdacaf62e04d705ac7
Roselle <Rosellewind@gmail.com>
2016-05-15 16:54:06 -0600

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Roselle Milvich on 5/15/16.
+//  Copyright © 2016 Roselle Tanner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
